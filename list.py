7436db0fb243e8f06784194cb7260eaea44e8af9
ychoon94 <mrsiao2256@gmail.com>
2020-05-24 18:31:38 +0800

    updated list.py with remove and pop testing

--- list.py
+++ list.py
@@ -6,6 +6,8 @@
 # for i in range(len(Rainbow)):
 #    print(Rainbow[i])
 
+import sys
+
 a = []  # start an empty list
 # read number of element in the list
 n = int(input('how many item in the list?'))
@@ -15,3 +17,20 @@ for i in range(n):
     # or
     # a.appent(int(input()))
 print(a)
+integer = 10
+try:
+    if(a.pop(a.index(integer))):
+        print("Success")
+        print(a)
+except ValueError:
+    print("Please try again")
+
+#if(a.pop(index)):
+#    print("Success")
+#    print(a)
+#else:
+#    print("Fail")
+#if(a.remove(100)):
+#    print("success")
+#else:
+#    print("Fail")
